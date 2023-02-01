5893b5776b1643a368ddeda1e53e24e2f9ba598c
xingyunyang01 <892716392@qq.com>
2023-02-01 13:35:54 +0800

    删除无用代码

--- cgin/Model.go
+++ cgin/Model.go
@@ -1,22 +1,5 @@
 package cgin
 
-import (
-	"encoding/json"
-	"log"
-)
-
 type Model interface {
 	String() string
-}
-
-// 支持多Model的形式，之所以把Models的类型定义为string，而不是[]Model，主要是为了简单
-type Models string
-
-func MakeModels(v interface{}) Models { //将[]Model序列化成[]byte，之后转成string
-	b, err := json.Marshal(v)
-	if err != nil {
-		log.Println(err)
-	}
-
-	return Models(b)
 }
